a0a076015d28b2641929730db7e0ca21ab1269d5
Lakshman Mallavarapu <lakshman.mallavarapu@beadevops.com>
2025-04-17 12:22:58 +1000

    site change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/vividlens-site/', // 👈 Add this line with your repo name
   plugins: [react()],
 });
