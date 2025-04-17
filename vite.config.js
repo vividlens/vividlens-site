8ad248f70dd0741372340503b00cd45bef979412
vividlens <vividlens.pic@gmail.com>
2025-04-17 15:27:32 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/vividlens-site/', // 👈 Add this line with your repo name
   plugins: [react()],
 });
